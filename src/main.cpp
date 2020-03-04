bc72a3f30d954a92a77f9959649b52d9cc6baa57
DasVoelkel <voelkel.dominik@gmail.com>
2020-03-04 22:18:48 +0100

    normal window!

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,32 @@
-#include <iostream>
-#include "main.hpp"
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <main.hpp>
 
-int main(){
-    print_hw();
+int main(int argc, char **argv){
+   
+   ALLEGRO_DISPLAY *display = NULL;
 
-return 0;
+   if(!al_init()) {
+      fprintf(stderr, "failed to initialize allegro!\n");
+      return -1;
+   }
+
+   display = al_create_display(640, 480);
+   if(!display) {
+      fprintf(stderr, "failed to create display!\n");
+      return -1;
+   }
+
+   al_clear_to_color(al_map_rgb(0,0,0));
+   
+   al_flip_display();
+
+   al_rest(10.0);
+
+   al_destroy_display(display);
+
+   return 0;
 }
-
 
 
 void print_hw(){
