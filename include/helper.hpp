34cf179ff295f72ca6208144351b9bc4416ad7b2
DasVoelkel <voelkel.dominik@gmail.com>
2020-03-08 23:15:44 +0100

    working game, restructuring

---
+++ include/helper.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+void must_init(bool test, const char *description);
+
+int between(int lo, int hi);
+float between_f(float lo, float hi);
+bool collide(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2);
