d81447708e355bd05310120d25c001a6a2a8d694
ZweiEuro <voelkel.dominik@gmail.com>
2020-04-23 18:46:10 +0200

    pin scaffolding events to display queue not input

--- include/gameInput.hpp
+++ include/gameInput.hpp
@@ -12,7 +12,7 @@ namespace game_input
 #define KEY_SEEN 1
 #define KEY_RELEASED 2
 
-void start(ALLEGRO_EVENT_SOURCE *event_source, ALLEGRO_EVENT_SOURCE *display_event_source);
+void start(ALLEGRO_EVENT_SOURCE *event_source);
 void stop();
 
 ALLEGRO_THREAD *get_p_input_thread();
